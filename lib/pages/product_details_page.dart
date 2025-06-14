8d1d731ac8bd8ec190b7c251c6c35a6f060da41b
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-06-14 12:26:09 +0600

    .

--- lib/pages/product_details_page.dart
+++ lib/pages/product_details_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shopping_app/cart_provider.dart';
 
-
 class ProductDetailsPage extends StatefulWidget {
   final Map<String, Object> product;
 
